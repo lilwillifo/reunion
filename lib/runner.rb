26a87f1bc07e9609aa747bc352864eb6a17a34bf
lilwillifo <margaretwilliford4@gmail.com>
2018-02-21 10:42:36 -0700

    runner in progress

--- lib/runner.rb
+++ lib/runner.rb
@@ -4,7 +4,32 @@ require 'erb'
 require 'pry'
 
 class Runner
-  template = File.read './reunion_details.erb'
-  erb_template = ERB.new template
+  def initialize
+    template = File.read './reunion_details.erb'
+    @erb_template = ERB.new template
+    create_reunion
+  end
 
+  def create_reunion
+    reunion = Reunion.new('Charlottesville')
+    reunion.add_activity('hiking', 45.00)
+    reunion.add_activity('frolicking', 90.00)
+    add_participants(reunion)
+  end
+
+  def add_participants(reunion)
+    reunion.activities[0].add_participant(name: 'Margaret', paid: 10.00)
+    reunion.activities[0].add_participant(name: 'Matt', paid: 20.00)
+    reunion.activities[0].add_participant(name: 'Kelly', paid: 15.00)
+
+    reunion.activities[1].add_participant(name: 'Margaret', paid: 20.00)
+    reunion.activities[1].add_participant(name: 'Matt', paid: 30.00)
+    reunion.activities[1].add_participant(name: 'Kelly', paid: 40.00)
+    build_template(reunion)
+  end
+
+  def build_template(reunion)
+    activities = reunion.activities
+    website = @erb_template.result(binding)
+  end
 end
