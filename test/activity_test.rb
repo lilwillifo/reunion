2e874956921a44e8f05aaab6b4a9d264584fcfd9
lilwillifo <margaretwilliford4@gmail.com>
2018-02-21 09:11:39 -0700

    initial commit

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,13 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new('hiking')
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new('hiking')
+    assert_equal 'hiking', activity.name
+  end
 end
